bc8b132bd9878e2b8af498f0e5e4d6c2c71c1e0e
y-yildirim <y.yildirim.ce@gmail.com>
2023-02-22 01:37:34 +0300

    Add Entities

---
+++ Core/ECommerce.Domain/Entities/Common/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace ETicaretAPI.Domain.Entities.Common
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}

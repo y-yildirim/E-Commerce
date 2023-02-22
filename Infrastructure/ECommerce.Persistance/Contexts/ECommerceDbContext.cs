5d670af0f3894f777ba9207089b92495f78dd0b3
y-yildirim <y.yildirim.ce@gmail.com>
2023-02-22 03:41:21 +0300

    Add and Configure DbContext for PostgreSQL

---
+++ Infrastructure/ECommerce.Persistance/Contexts/ECommerceDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ETicaretAPI.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceApp.Persistence.Contexts
+{
+    public class ECommerceDbContext : DbContext
+    {
+        public ECommerceDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Infrastructure/ECommerce.Persistance/DesignTimeDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using ECommerceApp.Persistence;
+using ECommerceApp.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace ETicaretAPI.Persistence
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ECommerceDbContext>
+    {
+        public ECommerceDbContext CreateDbContext(string[] args)
+        {
+            DbContextOptionsBuilder<ECommerceDbContext> dbContextOptionsBuilder = new();
+            dbContextOptionsBuilder.UseNpgsql(Configuration.ConnectionString);
+            return new(dbContextOptionsBuilder.Options);
+        }
+    }
+}
